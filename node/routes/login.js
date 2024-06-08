6ade4b20d906c47f87a48dd8690f579dea0833a3
JoeTCode <joejoe98t@gmail.com>
2024-06-08 15:36:53 +0100

    hardcoded admin object into login route

--- node/routes/login.js
+++ node/routes/login.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 require('../passport-config');
 const passport = require('passport');
+const bcrypt = require('bcrypt');
+const pool = require('../database');
 
 function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
@@ -10,7 +12,24 @@ function checkNotAuthenticated(req, res, next) {
     return next();
 }
 
-router.get('/', checkNotAuthenticated, function(req, res, next) { 
+router.get('/', checkNotAuthenticated, async function(req, res, next) {
+    const hashedPassword = await bcrypt.hash('pass', 10);
+    try {
+        await pool.query('INSERT INTO admins (name, password, phone_number) VALUES ($1, $2, $3)', ['admin1', hashedPassword,'+855 22 324 3949'])
+    } catch (err) {
+        if (err.code === '23505') {
+            console.log('');
+        }
+    }
+    try {
+        const res = await pool.query('SELECT * FROM admins');
+        console.log(res.rows);
+    } catch (err) {
+        if (err.code === '23505') {
+            console.log('');
+        }
+    }
+    
     res.render('login', { title: 'Express' });
     }
 );
