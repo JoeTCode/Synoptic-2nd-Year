60c8d7c578d6794ca152096db38fefd72befc628
JoeTCode <joejoe98t@gmail.com>
2024-06-08 15:58:12 +0100

    Added README and some comments

--- node/routes/login.js
+++ node/routes/login.js
@@ -9,14 +9,20 @@ const pool = require('../database');
 const { passport, checkNotAuthenticated } = require('../passport-config');
 
 router.get('/', checkNotAuthenticated, async function(req, res, next) { 
-    const hashedPassword = await bcrypt.hash('pass', 10);
+    // inserting ADMIN object with NAME: admin1
+    // PASSWORD: pass
+    // PHONE NUMBER: +855 22 324 3949
+    // upon routing to login
+    
     try {
+        const hashedPassword = await bcrypt.hash('pass', 10);
         await pool.query('INSERT INTO admins (name, password, phone_number) VALUES ($1, $2, $3)', ['admin1', hashedPassword,'+855 22 324 3949'])
     } catch (err) {
         if (err.code === '23505') {
             console.log('');
         }
     }
+    // insert ends here
     res.render('login', { title: 'Login' });
     }
 );
