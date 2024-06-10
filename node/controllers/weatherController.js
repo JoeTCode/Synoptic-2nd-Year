291d2066dd3237419bfd64cbf357c45450c9fd14
JoeTCode <joejoe98t@gmail.com>
2024-06-10 01:59:46 +0100

    Added Flood Warnings
    Takes rates of discharge from rivers near the chosen location, and produces alert messages based on the rate. For now it only logs the messages to console, will need to implement a way to send messages if the date of the flood warning is in the future

--- node/controllers/weatherController.js
+++ node/controllers/weatherController.js
@@ -40,6 +40,7 @@ async function getWeatherData() {
         };
 
         // `weatherData` now contains a simple structure with arrays for datetime and weather data
+        console.log('Weather data');
         for (let i = 0; i < weatherData.hourly.time.length; i++) {
             console.log(
                 weatherData.hourly.time[i].toISOString(),
@@ -50,3 +51,100 @@ async function getWeatherData() {
         console.error("Error fetching weather data:", error);
     }
 }
+
+//getWeatherData(); // commented out for time being
+
+// below is the api code to grab the nearby rivers discharge rate (rate of flow in m/s^3)
+// the latitude and logitude provided matches with Pu Ngaol
+const flood_params = {
+    "latitude": 12.57633758416416,
+    "longitude": 106.93316285065214,
+    "daily": ["river_discharge", "river_discharge_mean"]
+};
+
+const flood_url = 'https://flood-api.open-meteo.com/v1/flood';
+
+async function getFloodData() {
+    try {
+        const responses = await fetchWeatherApi(flood_url, flood_params);
+
+        // Helper function to form time ranges
+        const range = (start, stop, step) =>
+            Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+        // Process first location. Add a for-loop for multiple locations or weather models
+        const response = responses[0];
+
+        // Attributes for timezone and location
+        const utcOffsetSeconds = response.utcOffsetSeconds();
+        const timezone = response.timezone();
+        const timezoneAbbreviation = response.timezoneAbbreviation();
+        const latitude = response.latitude();
+        const longitude = response.longitude();
+
+        const daily = response.daily();
+
+        // Note: The order of weather variables in the URL query and the indices below need to match!
+        const weatherData = {
+            daily: {
+                time: range(Number(daily.time()), Number(daily.timeEnd()), daily.interval()).map(
+                    t => new Date((t + utcOffsetSeconds) * 1000)
+                ),
+                riverDischarge: daily.variables(0).valuesArray(),
+                riverDischargeMean: daily.variables(1).valuesArray(),
+            },
+        };
+
+        // instead of logging all the data in a loop (commented code before catch block),
+        // it is returned as an object to be manipulated in the function below this one
+        console.log('Flood data');
+        return { date: weatherData.daily.time,
+                discharge: weatherData.daily.riverDischarge,
+                mean_discharge: weatherData.daily.riverDischargeMean
+            }
+
+        // for (let i = 0; i < weatherData.daily.time.length; i++) {
+        //     console.log(
+        //         weatherData.daily.time[i].toISOString(),
+        //         weatherData.daily.riverDischarge[i],
+        //         weatherData.daily.riverDischargeMean[i]
+        //     );
+        // }
+
+    } catch (error) {
+        console.error('Error fetching flood data:', error);
+    }
+}
+
+async function produceFloodWarningMessage() {
+    data = await getFloodData();
+    let array = []; // holds the total areas of high discharge rates
+    let sub_array = []; // holds areas of high river discharge rates
+    // flood_risk_multiplier determines what is considered a 'high river discharge rate' aka flood risk.
+    // 1.5 times multiplier was chosen as an estimation for a flood risk, can be changed if there is a better
+    // prediction.
+    const flood_risk_multiplier = 1.5;
+    for (let i=0; i<data.mean_discharge.length; i++) {
+        if (data.discharge[i] >= data.mean_discharge[i]*flood_risk_multiplier) {
+            sub_array.push({'date': data.date[i], 'discharge': data.discharge[i]});
+        }
+        if (data.discharge[i] < data.mean_discharge[i]*flood_risk_multiplier) {
+            if (sub_array.length !== 0) {
+                array.push(sub_array);
+            }
+            sub_array = [];
+        }
+    }
+    // log statement for test purposes
+    console.log(array);
+    // selects the first and last value of each discharge peak, and formats the values into a message string
+    array.forEach(discharge_peak => {
+        const discharge_peak_object = discharge_peak[0];
+        const start_date = discharge_peak_object.date;
+        const end_date = discharge_peak[discharge_peak.length-1].date;
+        console.log(`Warning, high chance of flood on: ${start_date} to: ${end_date}`);
+    })
+}
+
+produceFloodWarningMessage();
+
