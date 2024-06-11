659a9b6d1f674064379eeef0ea3527773c39f68d
JoeTCode <joejoe98t@gmail.com>
2024-06-11 15:38:54 +0100

    Reformatted Ollie's implementation

--- node/app.js
+++ node/app.js
@@ -67,6 +67,9 @@ app.use('/send-message', sendMessageRouter);
 app.use('/settings', settingsRouter);
 app.use('/weather', weatherRouter);
 app.use('/twilio', twilioRouter);
+app.get('/weather-test', (req, res) => {
+  res.render('WeatherPage');
+})
 
 app.post('/logout', (req, res) => {
   req.logout(function(err) {
